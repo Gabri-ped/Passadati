db77cfbe7025be933f471a119724b98b99393dad
Gabriele <gabri.pe25@gmail.com>
2025-04-28 12:45:19 +0200

    Esercizio1 W2

---
+++ Assets/Scripts/Esercizio1.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Esercizio1 : MonoBehaviour
+   
+{
+    public int index = 10;
+    public int num = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        while(index > 0)
+        {
+            num++;
+            Debug.Log("Il valore di num è " + num);
+                index--;
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
